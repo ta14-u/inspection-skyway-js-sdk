7c588e505a5688b73be666833c4e485cf0365cbc
ta14-u <57866052+ta14-u@users.noreply.github.com>
2023-05-08 11:12:29 +0900

    test: fix jest config moduleNameMapper

--- examples/large-room/jest.config.js
+++ examples/large-room/jest.config.js
@@ -1,5 +1,13 @@
 /** @type {import('jest').Config} */
-module.exports = {
+const config = {
   preset: 'ts-jest',
   testEnvironment: 'jsdom',
+  moduleNameMapper: {
+    // `SyntaxError: Unexpected token 'export'` が発生するので uuid を強制的に CJS で読み込む.
+    // skyway パッケージ内で使用している uuid モジュールを ^9.0.0 にすれば解決しそう
+    // @see https://stackoverflow.com/questions/73203367/jest-syntaxerror-unexpected-token-export-with-uuid-library
+    uuid: require.resolve('uuid'),
+  },
 };
+
+module.exports = config;
