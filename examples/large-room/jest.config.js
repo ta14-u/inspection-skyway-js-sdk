78df19e497355f131a507f805ed44995cc1b52ef
ta14-u <57866052+ta14-u@users.noreply.github.com>
2023-05-08 11:34:29 +0900

    refactor: update commnet

--- examples/large-room/jest.config.js
+++ examples/large-room/jest.config.js
@@ -4,8 +4,8 @@ const config = {
   testEnvironment: 'jsdom',
   moduleNameMapper: {
     // `SyntaxError: Unexpected token 'export'` が発生するので uuid を強制的に CJS で読み込む.
-    // skyway パッケージ内で使用している uuid モジュールを ^9.0.0 にすれば解決しそう
     // @see https://stackoverflow.com/questions/73203367/jest-syntaxerror-unexpected-token-export-with-uuid-library
+    // skyway パッケージ内で使用している uuid モジュールを ^9.0.0 にすれば解決しそう
     uuid: require.resolve('uuid'),
   },
 };
